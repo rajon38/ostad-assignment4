0d2f243d5967c64fadbdc2d2c89afc3bffc4b1b6
RASHEDUL ISLAM <rashedul.rajon@gmail.com>
2022-12-05 17:04:51 +0600

    updated

--- server.js
+++ server.js
@@ -52,10 +52,11 @@ const storage= multer.diskStorage({
 })
 
 const fileFilter = (req,file,cb)=>{
-    if(file.mimetype==="image/jpg" || file.mimetype==="image/png"){
+    if(file.mimetype==="image/jpeg" || file.mimetype==="image/png"){
         cb(null, true);
     }else {
-        cb(null, false);
+        cb(null, false)
+        return cb(new Error('Only .png, .jpg and .jpeg format allowed!'));
     }
 }
 
